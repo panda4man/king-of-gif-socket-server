4d1043bbc37582274ec5448f4c9fcd14f8205d26
Andrew Clinton <ddrew.clinton@gmail.com>
2017-12-08 14:49:42 -0500

    make model create random room code

--- src/models/room.js
+++ src/models/room.js
@@ -1,9 +1,11 @@
 import Model from './model'
+import crypto from 'crypto-random-string'
 
 export default class Room extends Model {
 	constructor() {
 		super();
 
 		this.fillable = ['code', 'limit'];
+		this.code = crypto(5);
 	}
 }
