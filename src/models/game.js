660ef715f1ea1789ab48984a225c37eaee588bb9
Andrew Clinton <ddrew.clinton@gmail.com>
2017-12-10 21:35:27 -0500

    added more props to game model

--- src/models/game.js
+++ src/models/game.js
@@ -5,7 +5,30 @@ export default class Game extends Model {
 	constructor() {
 		super();
 
-		this.fillable = ['room', 'round'];
+		this.fillable = ['room'];
 		this.id = crypto(6);
+		this.rounds = [];
+		this.players = [];
+
+		let roundOne = {
+			started: false,
+			questions: [],
+			questionsSubmitted: false,
+			responses: [],
+			responsesSubmitted: false,
+			completed: false
+		};
+
+		let roundTwo = {
+			started: false,
+			questions: [],
+			questionsSubmitted: false,
+			responses: [],
+			responsesSubmitted: false,
+			completed: false
+		};
+
+		this.rounds.push(roundOne);
+		this.rounds.push(roundTwo);
 	}
 }
